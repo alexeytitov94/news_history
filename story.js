fff71a649953581041450c97ef3e0483f20f3e89
Alexey <alexeytitov94@mail.ru>
2020-04-17 15:19:03 +0300

    0.0.1

---
+++ story.js
@@ -0,0 +1,71 @@
+let getStories = [
+        Zuck.buildTimelineItem(
+          "NYT", 
+          "https://pbs.twimg.com/profile_images/1098244578472280064/gjkVMelR_400x400.png",
+          "NYT",
+          "https://ramon.codes",
+          timestamp(),
+          [
+            [
+              "NYT-1", 
+              "photo", 
+              3, 
+              "https://static01.nyt.com/images/2020/04/15/business/16virus-nat-briefing-china/merlin_171601788_96d21fb3-835b-4044-be59-f7aeafa2af15-superJumbo.jpg?quality=90&auto=webp",
+              
+              'https://www.nytimes.com/2020/04/16/us/coronavirus-cases-live-updates.html', 
+              'China’s economy shrinks, ending nearly half a century of continuous growth.'
+              false, 
+              false, 
+              timestamp()
+            ]
+          ]
+        ),
+        Zuck.buildTimelineItem(
+          "CNN",
+          "https://cdn.cnn.com/cnn/.e/img/3.0/global/misc/cnn-logo.png",
+          "CNN",
+          "",
+          timestamp(),
+          [
+            [
+              "gorillaz-2", "photo", 3, "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/5.jpg", "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/5.jpg", '', false, false, timestamp()
+            ],
+          ]
+        ),
+        Zuck.buildTimelineItem(
+          "The Guardian",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9PHJy7gseMJLccrvL_ZzK873HTfOcmlUH0MKYcyexnZxwAIdw&usqp=CAU",
+          "The Guardian",
+          "",
+          timestamp(),
+          [
+            [
+              "ladygaga-2", "photo", 3, "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/7.jpg", "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/7.jpg", 'http://ladygaga.com', false, false, timestamp()
+            ],
+          ]
+        ),
+        Zuck.buildTimelineItem(
+          "Reuters",
+          "https://imgclf.112.ua/original/2016/04/01/221364.jpeg?timestamp=1459491090",
+          "Reuters",
+          "",
+          timestamp(),
+          [
+            [
+              "starboy-1", "photo", 5, "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/8.jpg", "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/8.jpg", '', false, false, timestamp()
+            ]
+          ]
+        ),
+        Zuck.buildTimelineItem(
+          "WSJ",
+          "https://imgclf.112.ua/original/2016/09/29/262269.png?timestamp=1475135285",
+          "WSJ",
+          "",
+          timestamp(),
+          [
+            [
+              "riverscuomo", "photo", 10, "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/9.jpg", "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/9.jpg", '', false, false, timestamp()
+            ]
+          ]
+        )
+      ]
